c8c9d1c0401ec252910d7ce14ec4d1e97d141622
Greg Oppman <goppman@gmail.com>
2015-03-08 21:21:32 -0400

    initial commit

---
+++ app/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version',
+  'myApp.notes'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise
+  ({
+    redirectTo: '/notes',
+    controller: 'NotesController'
+  });
+
+}]);
